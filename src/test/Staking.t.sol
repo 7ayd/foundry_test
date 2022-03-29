2c0dd75db6bf648cfd42c6bf99040ddba162cce9
Zayd Alashini <86687700+7ayd@users.noreply.github.com>
2022-03-28 20:05:21 -0700

    first commit

---
+++ src/test/Staking.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.10;
+
+import "ds-test/test.sol";
+import "../Staking.sol";
+import "./mocks/MockERC20.sol";
+
+contract StakingTest is DSTest {
+    StakingContract public Staking;
+    MockERC20 public MockERC20;
+
+    function setUp() public {
+        stakingContract = new StakingContract();
+        mockERC = new MockERC20();
+    }
+
+    function testExample() public {
+        uint256 amount = 10e18;
+        mockERC.approve(address(stakingContract), amount);
+        bool stakingPassed = stakingContract.stake(amount, address(mockERC));
+        assertTrue(stakingPassed);
+    }
+}
